13183ef833aadb171b6364ba089cf5cb4c3e0a90
Mccartheney Mendes <mccartheney@hotmail.com>
2024-06-08 13:56:38 +0100

    folder creation css finalized

---
+++ static/js/dashboard/folders/toggleFileAndFolderCreation.js
@@ -0,0 +1,27 @@
+
+window.onload = () => {
+    let folderCreator = document.querySelector(".content_area_fileManagement_header_createArea_createFolder");
+
+    let typeOfCreation = document.querySelector(".content_area_fileManagement_header_createArea_dropDown_options")
+
+    document.querySelector(".toggle_dropDown").onclick = () => {
+
+        typeOfCreation.classList.toggle("invisible")
+    }
+
+
+    let folderButton = document.querySelector(".call_folderCreator");
+    folderButton.addEventListener("click", () => {
+
+        folderCreator.classList.toggle("hidden")
+        typeOfCreation.classList.toggle("invisible")
+
+    })
+
+    let close_folder_creations = document.querySelectorAll(".close_folder_creation");
+    close_folder_creations.forEach(element => {
+        element.onclick = () => {
+            folderCreator.classList.toggle("hidden");
+        }
+    });
+}
